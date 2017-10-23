82159ea2fa4758ed7c6a53967af31f7a758c5a75
Chris Stryczynski <chris@soundshare.co.uk>
2017-10-23 09:22:36 +0100

    Initial WIP - get path to section file

--- Main.hs
+++ Main.hs
@@ -1,16 +1,56 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS -Wno-unused-imports #-}
+{-# OPTIONS -Wno-unused-matches #-}
 
-import Turtle (empty, shellStrictWithErr, ExitCode)
-import Data.Text (Text)
-import Data.Text.IO as Text.IO (putStrLn)
+import Turtle hiding (f, e, x, o, s)
+--import Filesystem.Path.CurrentOS ( FilePath(FilePath) )
+import Data.Text (Text, lines)
 import Data.Monoid ((<>))
+import Prelude hiding (lines)
+import Safe
+import System.Posix.Directory
+import qualified Control.Foldl as Fold
+import Data.String.Conversions
+import Filesystem.Path.CurrentOS (encodeString, fromText)
+
+exitCodeToEither :: String -> ExitCode  -> Either String ()
+exitCodeToEither m e = case e of
+  ExitSuccess -> Right ()
+  _ -> Left m
 
 runSh :: Text -> IO (ExitCode, Text, Text)
-runSh x' = shellStrictWithErr ("git rev-list HEAD -- " <> x') empty
+runSh x = shellStrictWithErr x empty
+
+gitCheckout :: Text -> Text -> IO (ExitCode)
+gitCheckout c p = do
+  (r, _, _) <- runSh $ "git checkout " <> c <> " -- " <> p
+  return r
+
+gitPathCommitHash :: Text -> IO (Maybe Text)
+gitPathCommitHash x = do
+  (r, o, _) <- runSh $ "git rev-list HEAD -- " <> x
+  case (r) of
+    ExitSuccess -> case (headMay $ lines o) of
+      Nothing -> return $ Nothing
+      v' -> return $ v'
+    _ -> return Nothing
+
+compileSection :: Text -> IO (Either String Text)
+compileSection filePrefix = do
+  let v = find (prefix $ fromString $ dir ++ convertString filePrefix) (fromString dir)
+        where
+          dir = "sections/"
+  fP <- fold (v) Fold.head
+  case fP of
+    Just fP' -> do
+      return $ Right $ convertString $ encodeString $ fP'
+    Nothing -> return $ Left "Nope"
 
 main :: IO ()
 main = do
-  (e, v, v') <- runSh "Main.hs"
-  print e
-  Text.IO.putStrLn v
-  Text.IO.putStrLn v'
+  changeWorkingDirectory "/home/chris/Projects/Haskell/Articles/Rainbox"
+  let counter = "0"
+  compileSection (counter <> "_") >>= (\z -> case z of
+    Left e -> print e
+    Right r -> putStrLn $ convertString r
+    )
