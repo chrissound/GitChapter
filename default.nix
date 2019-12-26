7cd62fd9be843da39735813ecca3ee33fb89abd2
Chris S <chris@soundshare.co.uk>
2019-12-26 10:07:08 +0000

    fix nix build, pin nixpkgs, get non-broken version of pandoc-include-code

--- default.nix
+++ default.nix
@@ -1,7 +1,26 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc864" } :
+{ nixpkgs ? import <nixpkgs> {}
+, compiler ? "ghc865"
+, sources ? import ./nix/sources.nix
+} :
+
 let
-  myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
+  niv = import sources.nixpkgs {
+    overlays = [
+      (_ : _ : { niv = import sources.niv {}; })
+    ] ;
+    config = {};
+  };
+  pkgs = niv.pkgs;
+  myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
+      # broken again
+      # https://github.com/NixOS/nixpkgs/issues/42073
+      pandoc-include-code  = self.callCabal2nix "pandoc-include-code" (builtins.fetchGit {
+        url = "git@github.com:owickstrom/pandoc-include-code.git";
+        rev = "7e4d9d967ff3e3855a7eae48408c43b3400ae6f4";
+      })
+        {};
+
     };
   };
 in
